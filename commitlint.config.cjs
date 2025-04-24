4989673af1ea0a433301509411fcc87ac9bc6425
Adegboyega Joseph <adegboyegajosephdon@gmail.com>
2025-04-24 12:55:26 +0100

    Adding a new commit for my continuous integration.

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -3,8 +3,8 @@ module.exports = {
   rules: {
     'header-min-length': [2, 'always', 20],
     'header-case': [2, 'always', 'sentence-case'],
-    'header-full-stop': [2, 'always', '.'], // Now expects a period at the end
-    'type-empty': [0], // Disable type requirement
-    'subject-empty': [0], // Disable subject empty check (for leniency)
+    'subject-full-stop': [0], // Disable this conflicting built-in rule
+    'type-empty': [0], // Optional: allow skipping feat:, fix:, etc.
+    'subject-empty': [0], // Optional: allow messages without a subject
   },
 };
