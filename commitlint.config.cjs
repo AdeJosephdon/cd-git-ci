486a3737451a39d94d42e61a355f2ceba55bd5f7
Adegboyega Joseph <adegboyegajosephdon@gmail.com>
2025-04-24 12:22:44 +0100

    Adding a new commit for my understanding.

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,13 +1,10 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    // Require subject (header) to be at least 20 characters
     'header-min-length': [2, 'always', 20],
-
-    // Enforce capitalized start
     'header-case': [2, 'always', 'sentence-case'],
-
-    // Disallow ending with period
-    'header-full-stop': [2, 'never', '.'],
+    'header-full-stop': [2, 'always', '.'], // Now expects a period at the end
+    'type-empty': [0], // Disable type requirement
+    'subject-empty': [0], // Disable subject empty check (for leniency)
   },
 };
