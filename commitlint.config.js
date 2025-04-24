4252a900dd07d13f21009f95a95bfc324eca75a0
Adegboyega Joseph <adegboyegajosephdon@gmail.com>
2025-04-24 13:31:50 +0100

    Adding a new commit for my continuous integration.

---
+++ commitlint.config.js
@@ -0,0 +1,12 @@
+import config from '@commitlint/config-conventional';
+
+export default {
+  ...config,
+  rules: {
+    'header-min-length': [2, 'always', 20],
+    'header-case': [2, 'always', 'sentence-case'],
+    'subject-full-stop': [0],
+    'type-empty': [0],
+    'subject-empty': [0],
+  },
+};
