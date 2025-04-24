eff932fdf54bcf01a282b1191db68d0326f4ea6d
Adegboyega Joseph <adegboyegajosephdon@gmail.com>
2025-04-24 13:23:05 +0100

    Adding a new commit for my understanding.

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,10 +1,14 @@
-module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'header-min-length': [2, 'always', 20],
-    'header-case': [2, 'always', 'sentence-case'],
-    'subject-full-stop': [0], // Disable this conflicting built-in rule
-    'type-empty': [0], // Optional: allow skipping feat:, fix:, etc.
-    'subject-empty': [0], // Optional: allow messages without a subject
-  },
+module.exports = async () => {
+  const config = await import('@commitlint/config-conventional');
+
+  return {
+    ...config.default,
+    rules: {
+      'header-min-length': [2, 'always', 20],
+      'header-case': [2, 'always', 'sentence-case'],
+      'subject-full-stop': [0], // Disable this conflicting built-in rule
+      'type-empty': [0], // Optional: allow skipping feat:, fix:, etc.
+      'subject-empty': [0], // Optional: allow messages without a subject
+    },
+  };
 };
